e62ca077d6a219c0dab6f1eb9a771c06a3824f4e
Pavan R <101463985+Pavanpani31@users.noreply.github.com>
2026-02-05 23:10:42 +0530

    remove duplicates in string

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class RemoveDuplicate
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter to world to check the duplicates:");
+            string Input = Console.ReadLine();
+            removeDuplicate(Input);
+            Console.WriteLine("Press any key exist");
+            Console.ReadLine();
+
+        }
+        static void removeDuplicate(string str)
+        {
+            string result = string.Empty;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (!result.Contains(str[i]))
+                {
+                    result += str[i];
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
